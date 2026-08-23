81ab09ec1a5148489818414292f9267a5e4715fc
Ribeiro Pedro <phcribeiro123@gmail.com>
2026-08-22 23:37:24 -0300

    Visibilidades

---
+++ java-basico/src/Visibilidade/view.java
@@ -0,0 +1,13 @@
+package Visibilidade;
+
+public class view {
+    //todos as classes do projeto podem acessar ele dando import
+    public void ControleSaida(){
+        System.out.println("controle genérico de saída");
+    }
+
+    //so as classes do mesmo pacote (no caso visibilidade) podem usar esse metodos. Ele é Defaut(ou package protected)
+    void Entrgar(){
+        System.out.println("entrega genérica");
+    }
+}
