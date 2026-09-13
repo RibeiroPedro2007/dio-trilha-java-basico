cde5fa479622dd4c937ef58d4bd8ccfa709ca43b
Ribeiro Pedro <phcribeiro123@gmail.com>
2026-09-13 15:35:55 -0300

    controller do usuario

---
+++ my-first-web-api/src/main/java/dio/web/api/controller/UserController.java
@@ -0,0 +1,38 @@
+package dio.web.api.controller;
+
+import dio.web.api.model.Usuario;
+import dio.web.api.repositorys.UserRepositorys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepositorys repositorys;
+
+    @PostMapping("/usuarios")
+    public void post(@RequestBody Usuario usuario){
+        repositorys.save(usuario);
+    }
+    @PutMapping("/usuarios")
+    public void put(@RequestBody Usuario usuario){
+        repositorys.update(usuario);
+    }
+
+    @GetMapping("/users")
+    List<Usuario> getUsers(){
+        return repositorys.listAll();
+    }
+
+    @GetMapping("/usuario/{id}")
+    public Usuario getOne(@PathVariable("id") Integer id){
+        return repositorys.finById(id);
+    }
+
+    @DeleteMapping("/usuarios/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        repositorys.remove(id);
+    }
+}
