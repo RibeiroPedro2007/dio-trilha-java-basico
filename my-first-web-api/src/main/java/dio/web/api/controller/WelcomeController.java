bd950d073b8ae20f8d268c96b111cfd67e9f2c41
Ribeiro Pedro <phcribeiro123@gmail.com>
2026-09-12 21:13:11 -0300

    primeiro controller

--- my-first-web-api/src/main/java/dio/web/api/controller/WelcomeController.java
+++ my-first-web-api/src/main/java/dio/web/api/controller/WelcomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping
     public String welcome(){
-        return "Welcome to my Spring Boot Web Api";
+        return "Welcome to my Spring Boot Web Api ;-;";
     }
 }
