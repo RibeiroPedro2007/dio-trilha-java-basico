3efbf0e73ae077ab09ec3287237ea314605d770e
Ribeiro Pedro <phcribeiro123@gmail.com>
2026-09-22 22:52:18 -0300

    atualização da classe implements

--- lab-padroes-projeto-spring/src/main/java/one/dio/gof/service/imp/ClienteServiceImp.java
+++ lab-padroes-projeto-spring/src/main/java/one/dio/gof/service/imp/ClienteServiceImp.java
@@ -1,28 +1,51 @@
 package one.dio.gof.service.imp;
 
 import one.dio.gof.model.Cliente;
+import one.dio.gof.model.ClienteRepository;
+import one.dio.gof.model.Endereco;
+import one.dio.gof.model.EnderecoRepository;
 import one.dio.gof.service.ClienteService;
+import one.dio.gof.service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class ClienteServiceImp implements ClienteService {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
+
     @Override
     public Iterable<Cliente> buscarTodos(){
-        return null;
+        return clienteRepository.findAll();
     }
     @Override
     public Cliente buscarPorId(Long id){
-        return null;
+        Optional<Cliente> cliente= clienteRepository.findById(id);
+        return cliente.get();
     }
     @Override
     public void inserir(Cliente cliente) {
-
+        enderecoRepository.findById(cliente.getEndereco().getCep()).orElseGet(()->{
+            Endereco end =viaCepService.consultarCep(cliente.getEndereco().getCep());
+            enderecoRepository.save(end);
+            return end;});
+        cliente.setEndereco(cliente.getEndereco());
+        clienteRepository.save(cliente);
     }
     @Override
     public void atualizar(long id, Cliente cliente) {
-
+        Optional<Cliente> clientebd= clienteRepository.findById(id);
+        if(clientebd.isPresent()){
+            inserir(cliente);
+        }
     }
     @Override
     public void deletar(Long id) {
-
+        clienteRepository.deleteById(id);
     }
 }
