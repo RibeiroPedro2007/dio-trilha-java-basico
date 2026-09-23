f2b43e0221c864c0d7414215f8c9b062cf840e6e
Ribeiro Pedro <phcribeiro123@gmail.com>
2026-09-22 23:03:20 -0300

    Primeira api rest na nuvem

---
+++ satander-dev-week-2026/src/main/java/me/dio/SatanderDevWeek2026Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatanderDevWeek2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatanderDevWeek2026Application.class, args);
+	}
+
+}
