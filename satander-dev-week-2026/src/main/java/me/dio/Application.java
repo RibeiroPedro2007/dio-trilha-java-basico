4ec15c7a09db600656fb270e4f8e9aa578c432eb
Ribeiro Pedro <phcribeiro123@gmail.com>
2026-09-23 20:59:22 -0300

    acertos finais

--- satander-dev-week-2026/src/main/java/me/dio/Application.java
+++ satander-dev-week-2026/src/main/java/me/dio/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SatanderDevWeek2026Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SatanderDevWeek2026Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
