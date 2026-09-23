812ee392a603ce203ee6df7742dc0521f36fa5c0
Ribeiro Pedro <phcribeiro123@gmail.com>
2026-09-22 22:30:47 -0300

    controller e implements

--- lab-padroes-projeto-spring/src/main/java/one/dio/gof/service/ClienteService.java
+++ lab-padroes-projeto-spring/src/main/java/one/dio/gof/service/ClienteService.java
@@ -1,4 +1,15 @@
 package one.dio.gof.service;
 
-public class ClienteService {
+import one.dio.gof.model.Cliente;
+
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(long id, Cliente cliente);
+
+    void deletar(Long id);
 }
---
+++ lab-padroes-projeto-spring/src/main/java/one/dio/gof/service/imp/ClienteServiceImp.java
@@ -0,0 +1,28 @@
+package one.dio.gof.service.imp;
+
+import one.dio.gof.model.Cliente;
+import one.dio.gof.service.ClienteService;
+
+public class ClienteServiceImp implements ClienteService {
+
+    @Override
+    public Iterable<Cliente> buscarTodos(){
+        return null;
+    }
+    @Override
+    public Cliente buscarPorId(Long id){
+        return null;
+    }
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+    @Override
+    public void atualizar(long id, Cliente cliente) {
+
+    }
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
